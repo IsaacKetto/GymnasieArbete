29115b560b285c9b7f4f2ced33069f38bb35856b
melker-ostling-nit-johaneberg <melker.ostling@elev.ga.ntig.se>
2022-10-06 00:23:03 +0200

    Test slut
    Nu kan man även ändra alla BGRA värden och det sparas i bilden

--- test.py
+++ test.py
@@ -11,10 +11,26 @@ img = cv2.imread('brabild.png',1)
 img = cv2.cvtColor(img, cv2.COLOR_BGR2BGRA)
 #Datan visas i B G R A
 
-# show image
-print(img)
+
+
+#Change value in array
+i = 0
+while i < len(img):
+    
+    j = 0
+    while j < len(img[0]):
+        img[i][j][3] =  100
+        j += 1
+    i += 1
+
+
+
+
+
+# show and save image
+# print(img[0][0][3])
 cv2.imwrite("Bild.png", img)
-# cv2.imshow('image',img)
-# cv2.waitKey(0)
+cv2.imshow('image',img)
+cv2.waitKey(0)
 
 
