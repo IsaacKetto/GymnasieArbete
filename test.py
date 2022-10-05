7f8fdb350d9e4d38651d51a9504539f86590f1e6
melker-ostling-nit-johaneberg <melker.ostling@elev.ga.ntig.se>
2022-10-05 23:54:55 +0200

    Slut test
    Lyckats kunna öppna bilden och spara bilden med BGRA värden

--- test.py
+++ test.py
@@ -8,24 +8,13 @@ np.set_printoptions(threshold=sys.maxsize)
 
 
 img = cv2.imread('brabild.png',1)
- 
+img = cv2.cvtColor(img, cv2.COLOR_BGR2BGRA)
+#Datan visas i B G R A
+
 # show image
 print(img)
-cv2.imshow('image',img)
-cv2.waitKey(0)
+cv2.imwrite("Bild.png", img)
+# cv2.imshow('image',img)
+# cv2.waitKey(0)
 
 
-# img = Image.open("Brabild.png") 
-# imgData = np.array(img)
-
-
-# print(img.format)
-# print(img.size)
-# print(img.mode)
-# print(imgData)
-
-
-# img = imageToArray(img)
-# print(img)
-# This method will show image in any image viewer 
-# im.show() 
