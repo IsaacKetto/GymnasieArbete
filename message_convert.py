4a2a3330a2fe4d5faaf53370bcd05a6d4ce5f767
Oscar-Lofvendahl-nti-johanneberg <oscar.pipinolofvendahl@elev.ga.ntig.se>
2022-10-13 15:39:30 +0200

    fix
    fix

--- message_convert.py
+++ message_convert.py
@@ -1,10 +1,15 @@
 #Convert string to bytes with bytes(str, enc, error)
 message = "Oscar is the best"
 message_encoded = bytes(message, 'UTF-8')
+array_with_byte_message = []
 
 #Printing the encoded message
 print(message_encoded)
 
 #Decoding the message
 for bytes in message_encoded:
-    print(bytes, end = ' ')
+    array_with_byte_message.append(bytes)
+
+print(array_with_byte_message)
+
+#Convert bytes back to string
