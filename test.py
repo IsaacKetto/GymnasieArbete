77a9b1ba1652db69148c5201382b2a7ba0269322
melker-ostling-nit-johaneberg <melker.ostling@elev.ga.ntig.se>
2022-11-17 15:39:58 +0100

    cv2 fix

--- test.py
+++ test.py
@@ -7,7 +7,8 @@ import cv2
 np.set_printoptions(threshold=sys.maxsize)
 
 
-img = cv2.imread('Brabild.png',1)
+img = cv2.imread('Brabild.png', cv2.IMREAD_UNCHANGED)
+
 img = cv2.cvtColor(img, cv2.COLOR_BGR2BGRA)
 #Datan visas i B G R A
 
@@ -18,7 +19,7 @@ for row in img:
     for pixel in row:
         i = 0
         while i < 4:
-            # pixel[i] = 100
+            # pixel[3] = 100
             i += 1
 
 print(img[0][1])
@@ -30,7 +31,7 @@ print(img[0][0])
 
 # show and save image
 # print(img[0][0][3])
-# cv2.imwrite("Bild.png", img)
+# cv2.imwrite("testBild.png", img)
 cv2.imshow('image',img)
 cv2.waitKey(0)
 
